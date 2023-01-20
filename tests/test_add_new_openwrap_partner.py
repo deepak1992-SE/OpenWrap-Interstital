92bc632e8b4e6de49154fe321c5b1b199ef3358e
preety-narvekar <preety.narvekar@pubmatic.com>
2023-01-20 14:52:12 +0530

    SD-4311: Corrected unit test case, as device capabilities are not required for in-app

--- tests/test_add_new_openwrap_partner.py
+++ tests/test_add_new_openwrap_partner.py
@@ -802,14 +802,6 @@ class AddNewOpenwrapPartnerTests(TestCase):
     mock_get_users.get_user_id_by_email = MagicMock(return_value=14523)
     mock_get_placements.get_placement_ids_by_name = MagicMock(
       return_value=[1234567, 9876543])
-    mock_get_device_capabilities.get_device_capabilities = MagicMock(
-      return_value= {
-        'Mobile Apps': 5005,
-        'MRAID v1': 5001,
-        'MRAID v2': 5006,
-        'Phone calls': 5000
-      }
-    )
     mock_get_advertisers.get_advertiser_id_by_name = MagicMock(
       return_value=246810)
     mock_create_orders.create_order = MagicMock(return_value=1357913)
@@ -830,10 +822,9 @@ class AddNewOpenwrapPartnerTests(TestCase):
                                                lineitem_prefix = 'LI_123', bidder_code=bidder_code, prices=prices,
                                                setup_type = constant.IN_APP, creative_template = None, num_creatives=2,
                                                use_1x1=False, currency_code='USD', custom_targeting= None,
-                                               same_adv_exception= False, device_categories=None, device_capabilities=['Mobile Apps'], 
+                                               same_adv_exception= False, device_categories=None, device_capabilities=[], 
                                                roadblock_type= 'ONE_OR_MORE', slot = None,adpod_creative_durations=None)
 
-    mock_get_device_capabilities.get_device_capabilities.assert_called_once()   
     (mock_create_creatives.create_duplicate_creative_configs
       .assert_called_once_with(bidder_code[0], order, 246810,  sizes, 2, creative_file='creative_snippet_openwrap_in_app.html', prefix='INAPP_xyz', safe_frame=False))
     mock_licas.make_licas.assert_called_once_with([543210,987650], [54321,98765], durations=None, setup_type='IN_APP', size_overrides=[], slot=None)
@@ -865,14 +856,6 @@ class AddNewOpenwrapPartnerTests(TestCase):
     mock_get_users.get_user_id_by_email = MagicMock(return_value=14523)
     mock_get_placements.get_placement_ids_by_name = MagicMock(
       return_value=[1234567, 9876543])
-    mock_get_device_capabilities.get_device_capabilities = MagicMock(
-      return_value= {
-        'Mobile Apps': 5005,
-        'MRAID v1': 5001,
-        'MRAID v2': 5006,
-        'Phone calls': 5000
-      }
-    )
     mock_get_advertisers.get_advertiser_id_by_name = MagicMock(
       return_value=246810)
     mock_create_orders.create_order = MagicMock(return_value=1357913)
@@ -894,10 +877,9 @@ class AddNewOpenwrapPartnerTests(TestCase):
                                                lineitem_prefix = 'LI_123', bidder_code=bidder_code, prices=prices,
                                                setup_type = constant.IN_APP_VIDEO, creative_template = None, num_creatives=2,
                                                use_1x1=False, currency_code='USD', custom_targeting= None,
-                                               same_adv_exception= False, device_categories=None, device_capabilities=['Mobile Apps'], 
+                                               same_adv_exception= False, device_categories=None, device_capabilities=[], 
                                                roadblock_type= 'ONE_OR_MORE', slot = None,adpod_creative_durations=None)
 
-    mock_get_device_capabilities.get_device_capabilities.assert_called_once()
     mock_get_users.get_user_id_by_email.assert_called_once_with(email)
     mock_get_placements.get_placement_ids_by_name.assert_called_once_with(
       placements)
