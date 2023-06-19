44c26843cd5407d4bf678a9fba941671faa5f5fa
Pubmatic-Dhruv-Sonone <83747371+Pubmatic-Dhruv-Sonone@users.noreply.github.com>
2023-06-19 14:17:11 +0530

    OTT-1112: Upgrade the GAM API to version v202305 (#39)
    * OTT-1112: Upgrade the GAM API to version v202305
    
    * OTT-1112: Upgrading GAM API version

--- dfp/get_creative_template.py
+++ dfp/get_creative_template.py
@@ -19,10 +19,10 @@ def get_creative_template_by_name(template_name):
     dfp_client = get_client()
     # Initialize appropriate service.
     creative_template_service = dfp_client.GetService(
-      'CreativeTemplateService', version='v202211')
+      'CreativeTemplateService', version='v202305')
 
      # Create a statement to select creative templates.
-    statement = (ad_manager.StatementBuilder(version='v202211')
+    statement = (ad_manager.StatementBuilder(version='v202305')
      .Where('name = :name')
      .WithBindVariable('name', template_name))
 
